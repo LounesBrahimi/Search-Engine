3f53211ab99ceb0607c409c9c2b268e1022756a5
LounesBrahimi <lounes.brahimi@etu.sorbonne-universite.fr>
2022-01-09 14:55:19 +0100

    Selection of 1664 books that satisfy the condition of 10000 words minimum

--- searchengin_backend/constructDB.py
+++ searchengin_backend/constructDB.py
@@ -1,29 +1,36 @@
-import urllib.request
+import requests
+import html2text
+
+url_online_db = "https://gutendex.com/"
 
 def numberWordOfString(text : str):
     word_list = text.split()
     return len(word_list)
 
-def contructDB(bookId: int, sizeDB: int, minNumberWords: int):
+def contructDB(sizeDB: int, minNumberWords: int):
     numberBooksStocked : int = 0
-    textBook : str = ""
+    
+    page_num : int = 0
     while (numberBooksStocked < sizeDB):
-        bookIdStr = str(bookId)
-        try:
-            with urllib.request.urlopen("https://gutenberg.org/files/"+bookIdStr +"/"+
-                                bookIdStr+"-h/"+bookIdStr+"-h.htm") as url:
-                textBook = url.read().decode('utf-8')
+            page_num += 1
+            list_books = requests.get(url_online_db+'/books?mime_type=text&'+'page='+str(page_num))
+            list_books_json = list_books.json()
+            data = list_books_json['results']
+
+            for book in data :
+                textBook : str = ""
+                if (numberBooksStocked > sizeDB):
+                    break
+                url_book = book['formats']['text/html']
+                book_html = requests.get(url_book).text
+                h = html2text.HTML2Text()
+                h.ignore_links = True
+                textBook = h.handle(book_html)
                 numberWords : int = numberWordOfString(textBook)
                 if (numberWords >= minNumberWords):
                     numberBooksStocked += 1
-                    print(numberWords)
-                    print(len(textBook))
-        except:
-            pass
-        bookId += 1
 
 
 sizeDB : int = 10
 minNumberWords : int = 10000
-bookInitialId : int = 56667
-contructDB(bookInitialId, sizeDB, bookInitialId)
+contructDB(sizeDB, minNumberWords)
